61e5329a3dd8f7b806c007b7899f1bc0a1d15430
gfaraujo <guilherme.araujo@imd.ufrn.br>
2020-02-14 16:35:21 -0300

    Testing first automation script

--- exp.sh
+++ exp.sh
@@ -1,14 +1,36 @@
 #!/bin/bash
-#./main-centosV7 samples 5000 ephBonus 0  ephPopHistory 0 threads 40 cycles 5000 ephTime 30 ni 1 > eph00.txt
-#./main-centosV7 samples 5000 ephBonus 0.05 ephPopHistory 0 threads 40 cycles 5000 ephTime 30 ni 1 > eph005.txt
-#./main-centosV7 samples 5000 ephBonus 0.1 ephPopHistory 0 threads 40 cycles 5000 ephTime 30 ni 1 > eph01.txt
 
-for i in $(seq 0 999); 
+#Parametros
+# $1 job-name $2 seq $3 graphtype $4 numNodes
+# $5 numEdges $6 samples $7 ephBonus $8 ephPopHistory
+# $9 threads $10 cycles $11 ephTime $12 ni
+# $13 sampleId $14 printPartials $15 rBMA
+# $16 rBMB $17 bBA $18 bBB $19 folder
+# $20 outfile
+
+#SBATCH --job-name=$1
+#SBATCH --mem=128000
+#SBATCH --cpus-per-task=64
+##SBATCH --hint=compute_bound
+#SBATCH --exclusive
+#SBATCH --mail-user=guilherme.araujo@imd.ufrn.br
+#SBATCH --mail-type=ALL
+#SBATCH --time=2-0:0
+
+module load softwares/python/3.6-anaconda-5.0.1
+module load compilers/gnu/7.3
+
+FILE=main
+if [ -f "$FILE" ]; then
+	echo "$FILE exists"
+else
+	g++ main.cpp -o main -O3 -lpthread
+	chmod +x main
+fi
+
+for i in $(seq 0 $2); 
 do
-	python3 main.py --operation=newGraph --graphtype=ba --numNodes=500 --numEdges=4
-	./main samples 30 ephBonus 0.15 ephPopHistory 0 threads 3 cycles 1000 ephTime 30 ni 0 sampleId $i
-#	./main-centosV7 samples 5000 ephBonus 0.15 ephPopHistory 0 threads 40 cycles 5000 ephTime 30 ni 1
+	python3 main.py --operation=newGraph --graphtype=$3 --numNodes=$4 --numEdges=$5
+	./main samples $6 ephBonus $7 ephPopHistory $8 threads $9 cycles $10 ephTime $11 ni $12 sampleId $i printPartials $14 rBMA $15 rBMB $16 bBA $17 bBB $18 >> $19/$20
 done
-#./main-centosV7 samples 5000 ephBonus 0.2 ephPopHistory 0 threads 40 cycles 5000 ephTime 30 ni 1 > eph02.txt
-#./main-centosV7 samples 5000 ephBonus 0.25 ephPopHistory 0 threads 40 cycles 5000 ephTime 30 ni 1 > eph025.txt
-#./main-centosV7 samples 5000 ephBonus 0.3 ephPopHistory 0 threads 40 cycles 5000 ephTime 30 ni 1 > eph03.txt
+
