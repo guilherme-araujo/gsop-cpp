11608ee3fbc2171518ebc4d93aa74297487c64fb
Guilherme Araújo <guilherme.araujo@imd.ufrn.br>
2021-04-09 19:14:38 -0300

    Fix typo

--- experiments/20210409/figure-4b-100-500g/a5g5.sh
+++ experiments/20210409/figure-4b-100-500g/a5g5.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cd ./ag5g && sbatch ./exp.sh -betaA=0.95 -betaB=0.95 -threads=72 -ephTime=30 -nodes=100 && cd ..
+cd ./a5g5 && sbatch ./exp.sh -betaA=0.95 -betaB=0.95 -threads=72 -ephTime=30 -nodes=100 && cd ..
