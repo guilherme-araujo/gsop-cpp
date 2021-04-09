95fad372dd359f49f4d9ffbae3fcb606b6efecb2
Guilherme Araújo <guilherme.araujo@imd.ufrn.br>
2021-04-09 18:58:32 -0300

    Bash script line

--- experiments/20210409/figure-4b-100-1000s/a3g7.sh
+++ experiments/20210409/figure-4b-100-1000s/a3g7.sh
@@ -1,1 +1,2 @@
+#!/bin/bash
 cd ./a3g7 && ./exp.sh -betaA=0.95 -betaB=0.95 -threads=72 -ephTime=30 -nodes=100 && cd ..
--- experiments/20210409/figure-4b-400/a7g3.sh
+++ experiments/20210409/figure-4b-400/a7g3.sh
@@ -1,1 +1,2 @@
+#!/bin/bash
 cd ./a7g3 && ./exp.sh -betaA=0.95 -betaB=0.95 -threads=72 -ephTime=30 -nodes=400 && cd ..
