d6967042748ee02dd4c0097a5d113d147a37cb99
Guilherme Araújo <guilherme.araujo@imd.ufrn.br>
2021-05-02 22:00:28 -0300

    Exps with a only / cheater, and searching penalty

---
+++ experiments/20210501/sB098/a5g5/exp.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+#SBATCH --job-name=a5g5
+#SBATCH --mem=96000
+#SBATCH --cpus-per-task=48
+##SBATCH --hint=compute_bound
+#SBATCH --mail-user=guilherme.araujo@imd.ufrn.br
+#SBATCH --mail-type=ALL
+#SBATCH --time=24:0:0
+
+module load softwares/python/3.6-anaconda-5.0.1
+module load compilers/gnu/7.3
+
+#argument parsing
+for i in "$@"
+do
+case $i in
+    -betaA=*|--extension=*)
+    BETA_A="${i#*=}"
+    shift # past argument=value
+    ;;
+    -betaB=*|--searchpath=*)
+    BETA_B="${i#*=}"
+    shift # past argument=value
+    ;;
+    -ephTime=*|--lib=*)
+    EPHTIME="${i#*=}"
+    shift # past argument=value
+    ;;
+    -threads=*|--lib=*)
+    THREADNUM="${i#*=}"
+    shift # past argument=value
+    ;;
+    -nodes=*|--lib=*)
+    NODES="${i#*=}"
+    shift # past argument=value
+    ;;
+    *)
+          # unknown option
+    ;;
+esac
+done
+
+echo "BETA A  = ${BETA_A}"
+echo "BETA B  = ${BETA_B}"
+echo "EPHTIME = ${EPHTIME}"
+echo "THREADNUM = ${THREADNUM}"
+echo "NODES = ${NODES}"
+
+FILE=gsop
+
+if [ -f "$FILE" ]; then
+
+	echo "$FILE exists"
+
+	touch job-over.txt
+	echo "scale=2; 0/1000" | bc > job-percent.txt
+	for i in $(seq 1 1000);
+	do
+		python3 main.py --operation=newGraph --graphtype=ba --numNodes=$NODES --numEdges=4
+		./$FILE samples 1000 ephBonus 0.05 ephBonusB 0.05 ephStartRatio 0.6 ephBuildingRatio 0.6666 ephReusingRatio 0.1666 ephPopHistory 0 threads $THREADNUM cycles 5000 ephTime $EPHTIME ni 0 sampleId $i printPartials 1 rBMA 1 rBMB 1 bBA $BETA_A bBB $BETA_B sBA 0.98 sBB 0.98 >> a5g5.txt
+		echo "scale=2; $i/1000" | bc > job-percent.txt
+	done
+
+	echo 1 > job-over.txt
+
+else
+    echo "$FILE not found."
+fi
---
+++ experiments/20210501/sB098/test-run.md
@@ -0,0 +1,11 @@
+# Commands for first execution
+
+### Generate plots based on real data
+
+The data on this folder is incomplete. Download the full data from http://hungria.imd.ufrn.br/~gfaraujo/alemanha/article-nov-1/figure-4b/ in the folders a1g9, a3g7, a5g5, a7g3 and a9g1. Do not replace lineplot-4b.py.
+
+The command for registering the program must be like:
+
+```
+python /home/gfaraujo/testdata/figure-4b/lineplot-4b.py testParam=something
+```
